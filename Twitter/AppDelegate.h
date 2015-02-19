52c78177f45a28d7add2699b3a9c4e32e5fc3dad
Ken Szubzda <kszubzda@box.com>
2015-02-19 10:07:33 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Ken Szubzda on 2/19/15.
+//  Copyright (c) 2015 Ken Szubzda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Ken Szubzda on 2/19/15.
+//  Copyright (c) 2015 Ken Szubzda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
